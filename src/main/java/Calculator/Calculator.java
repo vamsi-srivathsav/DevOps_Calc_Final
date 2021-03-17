ef04ad93cfac54cc06dc2860ac6ef872ee66f6af
vamsisrivathsav-dev <vamsi.srivathsava@gmail.com>
2021-03-17 14:47:40 +0530

    basic out-line

---
+++ src/main/java/Calculator/Calculator.java
@@ -0,0 +1,21 @@
+
+package calculator;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+
+public class Calculator {
+
+
+    public Calculator() {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+
+
+}
